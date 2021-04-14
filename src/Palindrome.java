b163cb17a2c6f7ef6ae2302e9f233618836335a0
Roberto Contreras <rdcs1998@gmail.com>
2021-04-14 00:41:40 -0400

    made palindrome

---
+++ src/Palindrome.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+public class Palindrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print("Put in your word\n");
+		String sentence = scan.nextLine().toString();
+		
+		int result;
+		
+		result = Palindrome(sentence);
+		
+		if (result==0) {
+			System.out.println("Not Palindrome");
+		}
+		if (result==1) {
+			System.out.println("Palindrome");
+		}
+	}
+	
+	public static int Palindrome(String str) {
+		
+		int i, len = str.length();
+	    StringBuilder dest = new StringBuilder(len);
+
+	    for (i = (len - 1); i >= 0; i--){
+	        dest.append(str.charAt(i));
+	    }
+		
+		if (str.equals(dest.toString())){
+			return 1;
+		}
+		return 0;
+		
+	}
+
+}
